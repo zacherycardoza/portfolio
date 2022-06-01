a2cee1dca5564e270b99e29a61cfcc32db3a4e83
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2022-06-01 11:34:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.set("port", process.env.PORT || port);
 
 const server = http.createServer(app);
 
-server.listen(port);
-server.on("listening", () => console.log(`listening on port ${port}`));
+server.listen(process.env.PORT || port);
+server.on("listening", () => console.log(`listening on port ${process.env.PORT || port}`));
